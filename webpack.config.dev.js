e8e52bb2c2e030595be062ec10b42cabd9be65d1
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2019-10-15 21:23:14 +0900

    リファクタリング

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,13 +1,8 @@
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = function(options) {
   return {
     ...options,
-    resolve: {
-      ...options.resolve,
-      plugins: [new TsconfigPathsPlugin()]
-    },
     plugins: [ ...options.plugins, new Dotenv() ]
   };
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,5 @@
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-
 module.exports = function(options) {
   return {
-    ...options,
-    resolve: {
-      ...options.resolve,
-      plugins: [new TsconfigPathsPlugin()]
-    }
+    ...options
   };
 }
