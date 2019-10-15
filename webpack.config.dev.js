08ebb12a1f5a6633759cb3becf7ba3b2657f4feb
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2019-10-15 20:55:38 +0900

    dot-envをwebpack化

---
+++ webpack.config.dev.js
@@ -0,0 +1,16 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = function(options) {
+  return {
+    ...options,
+    resolve: {
+      ...options.resolve,
+      plugins: [new TsconfigPathsPlugin()]
+    },
+    plugins: [
+      ...options.plugins,
+      new Dotenv(),
+    ]
+  };
+}
