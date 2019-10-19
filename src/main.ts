f7716be3d61794cbed73a8b7d1e4112c5c5245ae
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2019-10-19 22:54:06 +0900

    VSCode用Launch用のスクリプト追加

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 
 import { SystemConfig } from '@sample/constants/system.config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  Logger.debug(`PORT: ${SystemConfig.PORT}`, 'bootstrap');
   await app.listen(SystemConfig.PORT);
 }
 bootstrap();
